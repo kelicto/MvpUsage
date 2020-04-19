0569d02742e3c9858f9f5c82d303d506fb4e36d6
KeLi <keliwork@outlook.com>
2020-04-19 22:13:53 +0800

    Add project files.

---
+++ KeLi.MvpUsage.App/Presenters/UserLoginCtrl.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * MIT License
+ *
+ * Copyright(c) 2020 KeLi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+             ,---------------------------------------------------,              ,---------,
+        ,----------------------------------------------------------,          ,"        ,"|
+      ,"                                                         ,"|        ,"        ,"  |
+     +----------------------------------------------------------+  |      ,"        ,"    |
+     |  .----------------------------------------------------.  |  |     +---------+      |
+     |  | C:\>FILE -INFO                                     |  |  |     | -==----'|      |
+     |  |                                                    |  |  |     |         |      |
+     |  |                                                    |  |  |/----|`---=    |      |
+     |  |              Author: KeLi                          |  |  |     |         |      |
+     |  |              Email: kelistudy@163.com              |  |  |     |         |      |
+     |  |              Creation Time: 04/19/2020 01:00:00 PM |  |  |     |         |      |
+     |  | C:\>_                                              |  |  |     | -==----'|      |
+     |  |                                                    |  |  |   ,/|==== ooo |      ;
+     |  |                                                    |  |  |  // |(((( [66]|    ,"
+     |  `----------------------------------------------------'  |," .;'| |((((     |  ,"
+     +----------------------------------------------------------+  ;;  | |         |,"
+        /_)_________________________________________________(_/  //'   | +---------+
+           ___________________________/___  `,
+          /  oooooooooooooooo  .o.  oooo /,   \,"-----------
+         / ==ooooooooooooooo==.o.  ooo= //   ,`\--{)B     ,"
+        /_==__==========__==_ooo__ooo=_/'   /___________,"
+*/
+
+using KeLi.MvpUsage.App.Business;
+using KeLi.MvpUsage.App.Models;
+using KeLi.MvpUsage.App.Views;
+
+namespace KeLi.MvpUsage.App.Presenters
+{
+    public class UserLoginCtrl
+    {
+        private readonly IUserLoginView _view;
+
+        public UserLoginCtrl(IUserLoginView view)
+        {
+            _view = view;
+        }
+
+        public void LoginSys()
+        {
+            if (string.IsNullOrWhiteSpace(_view.UserName))
+            {
+                _view.ShowMsg("User name cannot empty!");
+
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(_view.Password))
+            {
+                _view.ShowMsg("Password cannot empty!");
+
+                return;
+            }
+
+            if (!_view.ShowConfirm("Are you sure to login system?"))
+                return;
+
+            var mod = new SystemUserVM
+            {
+                UserName = _view.UserName,
+
+                Password = _view.Password
+            };
+
+            var ety = UserLoginSvr.GetItem(mod.UserName);
+
+            _view.ShowMsg(ety.Pwd == mod.Password ? "Logins successful!" : "Logins failed!");
+        }
+    }
+}
---
+++ KeLi.MvpUsage.App/Views/UserLoginView.cs
@@ -0,0 +1,97 @@
+﻿/*
+ * MIT License
+ *
+ * Copyright(c) 2020 KeLi
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+/*
+             ,---------------------------------------------------,              ,---------,
+        ,----------------------------------------------------------,          ,"        ,"|
+      ,"                                                         ,"|        ,"        ,"  |
+     +----------------------------------------------------------+  |      ,"        ,"    |
+     |  .----------------------------------------------------.  |  |     +---------+      |
+     |  | C:\>FILE -INFO                                     |  |  |     | -==----'|      |
+     |  |                                                    |  |  |     |         |      |
+     |  |                                                    |  |  |/----|`---=    |      |
+     |  |              Author: KeLi                          |  |  |     |         |      |
+     |  |              Email: kelistudy@163.com              |  |  |     |         |      |
+     |  |              Creation Time: 04/19/2020 01:00:00 PM |  |  |     |         |      |
+     |  | C:\>_                                              |  |  |     | -==----'|      |
+     |  |                                                    |  |  |   ,/|==== ooo |      ;
+     |  |                                                    |  |  |  // |(((( [66]|    ,"
+     |  `----------------------------------------------------'  |," .;'| |((((     |  ,"
+     +----------------------------------------------------------+  ;;  | |         |,"
+        /_)_________________________________________________(_/  //'   | +---------+
+           ___________________________/___  `,
+          /  oooooooooooooooo  .o.  oooo /,   \,"-----------
+         / ==ooooooooooooooo==.o.  ooo= //   ,`\--{)B     ,"
+        /_==__==========__==_ooo__ooo=_/'   /___________,"
+*/
+
+using System;
+using System.Windows.Forms;
+
+using KeLi.MvpUsage.App.Presenters;
+
+namespace KeLi.MvpUsage.App.Views
+{
+    public partial class UserLoginView : Form, IUserLoginView
+    {
+        private readonly UserLoginCtrl _pst;
+
+        public UserLoginView()
+        {
+            InitializeComponent();
+
+            _pst = new UserLoginCtrl(this);
+        }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public void ShowMsg(string msg)
+        {
+            MessageBox.Show(msg);
+        }
+
+        public bool ShowConfirm(string msg)
+        {
+            var result = MessageBox.Show(msg, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            return result == DialogResult.OK;
+        }
+
+        public void LoginSys()
+        {
+            _pst.LoginSys();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            UserName = txtUserName.Text.Trim();
+
+            Password = txtPwd.Text.Trim();
+
+            LoginSys();
+        }
+    }
+}
